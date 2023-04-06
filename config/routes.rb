5ee6f490e0af2521cb2678a01acb58c865327cd8
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-06 17:02:16 -0500

    Now users can't delete the profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :profiles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  resources :profiles
+    # GET     /profiles               profiles#index
+    # GET     /profiles/new           profiles#new
+    # POST    /profiles               profiles#create
+    # GET     /profiles/:id           profiles#show
+    # GET     /profiles/:id/edit      profiles#edit
+    # PUT     /profiles/:id           profiles#update
+    # DELETE  /profiles/:id           profiles#destroy
+
+
+  root "profiles#index"
+    #DEFINED THE ROOT PATH (html file is app/views/profiles/profile.html.erb)
 end
